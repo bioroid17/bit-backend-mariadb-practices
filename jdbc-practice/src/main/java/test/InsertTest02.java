becd1db703e56975988a7940a2f8f39653891663
bioroid17 <trace1997@ajou.ac.kr>
2023-07-10 10:02:21 +0900

    JDBC 프로그래밍 연습 2

---
+++ jdbc-practice/src/main/java/test/InsertTest02.java
@@ -0,0 +1,65 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class InsertTest02 {
+
+	public static void main(String[] args) {
+		insert("영업2");
+		insert("영업3");
+	}
+	
+	private static boolean insert(String deptName) {
+		boolean result = false;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			// 1. JDBC Driver Class 로딩
+			// 오타 없게 하자.
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			// 2. 연결하기
+			// 연결 정보를 getConnection에 줘야 함
+			// 오타 없게 하자.
+			// jdbc:mariadb://(서버 IP 주소):(mysql 포트 번호, 기본 3306)/(db이름)?옵션
+			String url = "jdbc:mariadb://192.168.0.162:3306/webdb?charset=utf8";
+			// 필요한 연결 정보: url, username, password
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+			// 3. Statement 준비
+			String sql = "insert into dept values (null, ?)";
+			pstmt = conn.prepareStatement(sql);
+			
+			// 4. 바인딩(binding)
+			// JDBC에서는 콜론을 붙이지 않는다!
+			pstmt.setString(1, deptName);
+			
+			// 5. SQL 실행
+			int count = pstmt.executeUpdate();
+			
+			// 6. 결과 처리
+			result = count == 1;
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:"+e);
+		} catch (SQLException e) {
+			System.out.println("Error:"+e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return result;
+	}
+}
